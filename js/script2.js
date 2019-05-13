8223f469b5aaac2dde87ef54f455f7038d2baee0
ildev <dev@industriallogic.com>
2019-05-13 16:21:05 -0700

    Implement jQuery ajax request

--- js/script2.js
+++ js/script2.js
@@ -1,18 +1,71 @@
 'use strict';
 
+// jQuery request
+// (() => {
+//   const url = 'http://api.openweathermap.org/data/2.5/weather?q=London,England';
+//   const apiKey = 'eac9eebd5253a65b267d683dfc52b619'; // Replace "APIKEY" with your own API key; otherwise, your HTTP request will not work
+//
+//   const updateUISuccess = response => {
+//     let condition = response.weather[0].main;
+//     let degC = response.main.temp - 273.15;
+//     let degCInt = Math.floor(degC);
+//     let degF = degC * 1.8 + 32;
+//     let degFInt = Math.floor(degF);
+//     // let weatherBox = document.querySelector('#weather');
+//     // weatherBox.innerHTML = `<p>${degCInt}&#176; C / ${degFInt}&#176; F</p><p>${condition}</p>`;
+//     let $weatherBox = $('#weather');
+//     $weatherBox.append(`<p>${degCInt}&#176; C / ${degFInt}&#176; F</p><p>${condition}</p>`);
+//   };
+//
+//   const updateUIError = () => {
+//     // let weatherBox = document.querySelector('#weather');
+//     // weatherBox.className = 'hidden';
+//     let $weatherBox = $('#weather');
+//     $weatherBox.addClass('hidden');
+//   };
+//
+//   $.get(`${url}&appid=${apiKey}`)
+//     .done(response => updateUISuccess(response))
+//     .fail(error => updateUIError(error));
+//
+//   console.log(this);
+//
+// })();
+
 // Fetch request
-(() => {
-  const url = 'http://api.openweathermap.org/data/2.5/weather?q=London,England';
-  const apiKey = 'eac9eebd5253a65b267d683dfc52b619'; // Replace "APIKEY" with your own API key; otherwise, your HTTP request will not work
+// (() => {
+//   const url = 'http://api.openweathermap.org/data/2.5/weather?q=London,England';
+//   const apiKey = 'eac9eebd5253a65b267d683dfc52b619'; // Replace "APIKEY" with your own API key; otherwise, your HTTP request will not work
+//
+//   fetch(`${url}&appid=${apiKey}`)
+//     .then(response => {
+//       if (!response.ok) throw Error(response.statusText);
+//       return response.json();
+//     })
+//     .then(response => {
+//       updateUISuccess(response);
+//     })
+//     .catch(error => {
+//       updateUIError();
+//     });
+//
+//   const updateUISuccess = response => {
+//     let condition = response.weather[0].main;
+//     let degC = response.main.temp - 273.15;
+//     let degCInt = Math.floor(degC);
+//     let degF = degC * 1.8 + 32;
+//     let degFInt = Math.floor(degF);
+//     let weatherBox = document.querySelector('#weather');
+//     weatherBox.innerHTML = `<p>${degCInt}&#176; C / ${degFInt}&#176; F</p><p>${condition}</p>`;
+//   };
+//
+//   const updateUIError = () => {
+//     let weatherBox = document.querySelector('#weather');
+//     weatherBox.className = 'hidden';
+//   };
+// })();
 
-  fetch(`${url}&appid=${apiKey}`)
-    .then(response => {
-      if (!response.ok) console.log('There was a problem... :\'(');
-      console.log('Response object ->', response);
-      console.log(response.json());
-    });
-})();
-
+// XHR request
 // (() => {
 //   const url = 'http://api.openweathermap.org/data/2.5/weather?q=London,England';
 //   const apiKey = 'eac9eebd5253a65b267d683dfc52b619'; // Replace "APIKEY" with your own API key; otherwise, your HTTP request will not work
